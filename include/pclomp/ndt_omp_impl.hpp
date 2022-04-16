7216fd28fe350a344ac7c3a208f91f86e64d1ae0
Ryohei Sasaki <rsasaki0109@gmail.com>
2022-04-16 18:02:31 +0900

    Fix sign of d1 in computing Hessian of NDT
    https://github.com/koide3/ndt_omp/pull/46

--- include/pclomp/ndt_omp_impl.hpp
+++ include/pclomp/ndt_omp_impl.hpp
@@ -356,7 +356,7 @@ pclomp::NormalDistributionsTransform<PointSource, PointTarget>::computeAngleDeri
 		h_ang_c2_ << (-sx * cz - cx * sy * sz), (sx * sz - cx * sy * cz), 0;
 		h_ang_c3_ << (cx * cz - sx * sy * sz), (-sx * sy * cz - cx * sz), 0;
 
-		h_ang_d1_ << (-cy * cz), (cy * sz), (sy);
+		h_ang_d1_ << (-cy * cz), (cy * sz), (-sy);
 		h_ang_d2_ << (-sx * sy * cz), (sx * sy * sz), (sx * cy);
 		h_ang_d3_ << (cx * sy * cz), (-cx * sy * sz), (-cx * cy);
 
